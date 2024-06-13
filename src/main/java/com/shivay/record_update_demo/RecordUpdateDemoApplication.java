af89ba6a77ae65785e2e00310e40dc14f5969868
raju-nichit <meetrajnichit@gmail.com>
2024-06-13 21:13:11 +0530

    Initial commit

---
+++ src/main/java/com/shivay/record_update_demo/RecordUpdateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shivay.record_update_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordUpdateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordUpdateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivay/record_update_demo/RecordUpdateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivay.record_update_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordUpdateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
